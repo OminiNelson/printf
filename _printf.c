04fdf81b6c534d82144c15089e99b07e60310823
Deji287 <slimdejavu0101@gmail.com>
2022-08-09 06:30:42 -0700

    Creating printf.c

--- _printf.c
+++ _printf.c
@@ -5,7 +5,7 @@ void print_buffer(char buffer[], int *buff_ind);
 /**
  * _printf - Printf function
  * @format: format
- * Author: Nelson and Oladimeji
+ * Author: Nelson Alexanderand  0motola Oladimeji
  * Return: Printed chars.
  */
 int _printf(const char *format, ...)
